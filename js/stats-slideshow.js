a7e615172962effe96cc2b39cff138fd76ffd0c7
Ivan Klus <ivanklus@live.com>
2018-01-19 14:49:11 -0500

    Changed text to stat

---
+++ js/stats-slideshow.js
@@ -0,0 +1,12 @@
+AFRAME.registerComponent('arrow-key-rotation', {
+  schema: {
+    textList: {type: 'string'},
+    waitTime: {type: 'float', default: 3}
+  },
+  init: function () {
+
+  },
+  play: function () {
+
+  }
+});
